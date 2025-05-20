36748fbdaa2d62be10fe7e010059577cd1acb2e7
Andreas Tennert <andreas@qupaya.com>
2025-05-20 11:50:31 +0200

    Use Lazy Loading for Book feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { BookComponent } from './book/book/book.component';
 import { AboutComponent } from './about/about.component';
-import { BookDetailComponent } from './book/book-detail/book-detail.component';
 
 export const routes: Routes = [
   {
@@ -11,11 +9,7 @@ export const routes: Routes = [
   },
   {
     path: 'books',
-    component: BookComponent
-  },
-  {
-    path: 'books/detail/:isbn',
-    component: BookDetailComponent
+    loadChildren: () => import('./book/book.routes').then(m => m.bookRoutes)
   },
   {
     path: 'about',
