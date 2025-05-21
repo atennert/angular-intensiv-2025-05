42e6c6868e895d81d715674b3deb09007912f602
Andreas Tennert <andreas@qupaya.com>
2025-05-21 12:27:55 +0200

    Write a custom validator for the Author field

--- src/app/book/book-new/book-new.component.ts
+++ src/app/book/book-new/book-new.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { BookForm } from './book-form.interface';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../book';
+import { authorValidator } from './author.validator';
 
 @Component({
   selector: 'app-book-new',
@@ -18,7 +19,7 @@ export class BookNewComponent {
 
   readonly form: FormGroup<BookForm> = this.fb.group<BookForm>({
     isbn: this.fb.control('', [Validators.required]),
-    author: this.fb.control('', [Validators.required]),
+    author: this.fb.control('', [Validators.required, authorValidator()]),
     title: this.fb.control('', [Validators.required]),
     subtitle: this.fb.control(''),
     abstract: this.fb.control(''),
